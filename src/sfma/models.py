ee563205f95a5c87b70863a00ef11193b29352fe
jizezhang <jizez@uw.edu>
2020-06-18 22:20:20 -0700

    modified model

--- src/sfma/models.py
+++ src/sfma/models.py
@@ -7,7 +7,7 @@
 """
 import numpy as np
 import pandas as pd
-from scipy.optimize import Bounds
+from scipy.optimize import LinearConstraint
 
 from anml.models.interface import Model
 from anml.parameter.parameter import ParameterSet
@@ -27,19 +27,20 @@ class LinearMixedEffectsMarginal(Model):
         return self._param_set
 
     @param_set.setter
-    def param_set(self, param_set_processed):
+    def param_set(self, param_set_processed: ParameterSet):
         self._param_set = param_set_processed
         self.n_betas = self._param_set.num_fe
         self.n_gammas = self._param_set.num_re_var
         self.x_dim = self.n_betas + self.n_gammas
         self.X = self._param_set.design_matrix
         self.Z = self._param_set.design_matrix_re
+        self.D = self._param_set.re_var_diag_matrix
         self.C = self._param_set.constr_matrix_full
         self.lb = self._param_set.constr_lower_bounds_full
         self.ub = self._param_set.constr_upper_bounds_full
-        self.bounds = Bounds(self.lb, self.ub)
+        self.constraint = LinearConstraint(self.C, self.lb, self.ub)
         
-        self.prior_fun = self._param_set.prior_fun     
+        self.prior_fun = self._param_set.prior_fun 
 
     def objective(self, x, data: Data):
         if self.param_set is None:
@@ -50,7 +51,7 @@ class LinearMixedEffectsMarginal(Model):
         gammas = x[self.n_betas:]
 
         Sigma = np.diag(data.obs_se)
-        Gamma = np.diag(gammas)
+        Gamma = np.diag(np.dot(self.D, gammas))
         y = data.obs 
 
         V = Sigma**2 + np.dot(self.Z,np.dot(Gamma, self.Z.T))
@@ -62,32 +63,35 @@ class LinearMixedEffectsMarginal(Model):
         return np.dot(self.X, betas)
 
     
-class SingleVariableMaximal(Model):
+class RandomEffectsOnly(Model):
 
-    def __init__(self, variable_processed: Variable = None):
-        self.variable = variable_processed
+    def __init__(self, param_set_processed: Variable = None):
+        self._param_set = param_set_processed
 
     @property
-    def variable(self):
-        return self._variable
+    def param_set(self):
+        return self._param_set
 
-    @variable.setter
-    def variable(self, variable_processed):
-        self._variable = variable_processed
-        self.Z = self.variable.design_matrix_re
-        self.C, self.lb, self.ub = self.variable.constraint_matrix_re()
-        self.prior_fun = collect_priors([self.variable.re_prior])
+    @param_set.setter
+    def param_set(self, param_set_processed):
+        self._param_set = param_set_processed
+        self.Z = self._param_set.design_matrix_re
+        self.C = self._param_set.constr_matrix_full[:, self._param_set.num_fe:]
+        self.lb = self._param_set.constr_lower_bounds_full
+        self.ub = self._param_set.constr_upper_bounds_full
+        self.constraint = LinearConstraint(self.C, self.lb, self.ub)
+        self.x_dim = self._param_set.num_re
 
-        self.bounds = Bounds(self.lb, self.ub)
+        self.prior_fun = self._param_set.prior_fun
 
     def objective(self, x, data):
-        if self.variable is None:
-            raise ValueError('Variable is not yet defined for this model.')
-        if len(x) != self.variable.n_groups:
-            raise TypeError(f'The length of x = {len(x)} is not equal to the number of groups {self.variable.n_groups}.')
+        if self.param_set is None:
+            raise ValueError('Parameter set is not yet defined for this model.')
+        if len(x) != self.param_set.num_re:
+            raise TypeError(f'The length of x = {len(x)} is not equal to the number of variables {self.x_dim}.')
         y = data.obs
         sigma = data.obs_se
-        return np.sum((y - x)**2 / (2*sigma**2)) + self.prior_fun(x)
+        return np.sum((y - np.dot(self.Z, x))**2 / (2*sigma**2)) + self.prior_fun(x)
 
     def predict(self, x):
         return np.dot(self.Z, x)
