89bb58190a0a6f643c0312b5a089f5374e8cee36
jizezhang <jizez@uw.edu>
2020-06-19 15:56:15 -0700

    modified solver

--- src/sfma/solvers.py
+++ src/sfma/solvers.py
@@ -6,7 +6,7 @@ from anml.solvers.interface import CompositeSolver, Solver
 from anml.solvers.base import ScipyOpt, ClosedFormSolver
 
 from sfma.data import Data
-from sfma.models import LinearMixedEffectsMarginal, RandomEffectsOnly
+from sfma.models import LinearMixedEffectsMarginal, GaussianRandomEffects
 
 
 class AlternatingSolver(CompositeSolver):
@@ -15,8 +15,8 @@ class AlternatingSolver(CompositeSolver):
         super().__init__(solvers_list)
         if len(self.solvers) < 3:
             self.lme_solver = ScipyOpt(LinearMixedEffectsMarginal())
-            self.u_solver = ScipyOpt(RandomEffectsOnly())
-            self.v_solver = ScipyOpt(RandomEffectsOnly())
+            self.u_solver = ClosedFormSolver(GaussianRandomEffects())
+            self.v_solver = ClosedFormSolver(GaussianRandomEffects())
 
     def fit(self, x_init: List[np.ndarray], data: Data, options: Optional[Dict[str, str]] = dict(maxiter=100, tol=1e-5)):
         betas, gammas, us, vs, eta = x_init 
@@ -25,14 +25,15 @@ class AlternatingSolver(CompositeSolver):
 
         lme_param_set = data.params[0]
         n_betas = lme_param_set.num_fe
-
         u_param_set = data.params[1]
         v_param_set = data.params[2]
 
         self.lme_solver.model.param_set = lme_param_set
+        self.u_solver.model.param_set = u_param_set
+        self.v_solver.model.param_set = v_param_set
         
-        itr, err = 0, 1.0
-        while itr < options['maxiter'] or err > options['tol']:
+        itr = 0
+        while itr < options['maxiter']:
             # fitting betas and gammas
             data.y = obs + np.dot(self.v_solver.model.Z, vs)
             self.lme_solver.fit(beta_gamma, data)
@@ -41,21 +42,21 @@ class AlternatingSolver(CompositeSolver):
             gammas = beta_gamma[n_betas:]
 
             # fitting us
-            u_variable.re_prior.std = np.dot(self.u_solver.D, gammas)
+            self.u_solver.gammas = gammas
             data.y -= np.dot(self.lme_solver.model.X, betas)
-            self.u_solver.model.variable = u_variable
             self.u_solver.fit(us, data)
             us = self.u_solver.x_opt
 
             # fitting vs
-            v_variable.re_prior.std = eta 
+            self.v_solver.gammas = [eta] 
             data.y = np.dot(self.lme_solver.model.X, betas) + np.dot(self.u_solver.model.Z, us) - obs 
-            self.v_solver.model.variable = v_variable
             self.v_solver.fit(vs, data)
             vs = self.v_solver.x_opt
 
             # fitting eta
             eta = [np.std(vs)]
+
+            itr += 1
 
 
 
