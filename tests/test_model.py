f7596b01fe864411293045864d8e7993edff5253
Peng Zheng <zhengp@uw.edu>
2022-04-12 13:09:35 -0700

    Integrate with "msca" package (#16)
    * Change to use the c2fun in msca
    
    * Switch to use the solver from msca package
    
    * Fix the beta assignment
    
    * Dynamically update the time in the documentation

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,13 +1,11 @@
 """
 Test the model module
 """
-import pytest
 import numpy as np
 import pandas as pd
-from regmod.prior import UniformPrior
-
+import pytest
+from sfma import Data, GaussianPrior, UniformPrior, Variable
 from sfma.model import SFMAModel
-from sfma import Data, Variable, GaussianPrior
 
 
 def ad_jacobian(fun, x, out_shape=(), eps=1e-10):
@@ -31,17 +29,19 @@ def ad_jacobian(fun, x, out_shape=(), eps=1e-10):
 
 
 @pytest.fixture
-def data():
+def df():
     np.random.seed(123)
-    df = pd.DataFrame({
+    return pd.DataFrame({
         "obs": np.random.randn(10),
-        "weights": 0.2 + np.random.rand(10),
-        "var": np.random.randn(10)
+        "obs_se": 0.2 + np.random.rand(10),
+        "var": np.random.randn(10),
+        "intercept": 1.0,
     })
-    return Data(col_obs="obs",
-                col_covs=["var"],
-                col_weights="weights",
-                df=df)
+
+
+@pytest.fixture
+def data():
+    return Data(obs="obs", obs_se="obs_se")
 
 
 @pytest.fixture
@@ -62,51 +62,50 @@ def variables(gprior, uprior):
     ]
 
 
-def test_mat(data, variables):
-    model = SFMAModel(data, variables)
-    assert np.allclose(model.mat, data.df[["intercept", "var"]].values)
+def test_mat(data, variables, df):
+    model = SFMAModel(data, variables, df=df)
+    assert np.allclose(model.mat, df[["intercept", "var"]].values)
 
 
-def test_gprior(data, variables):
-    model = SFMAModel(data, variables)
-    assert np.allclose(model.linear_gvec,
+def test_gprior(data, variables, df):
+    model = SFMAModel(data, variables, df=df)
+    linear_gvec = model.parameter.prior_dict["direct"]["GaussianPrior"].params
+    assert np.allclose(linear_gvec,
                        np.array([[0.0, 0.0], [np.inf, 1.0]]))
 
 
-def test_uprior(data, variables):
-    model = SFMAModel(data, variables, True, True)
-    assert np.allclose(model.linear_uvec,
-                       np.array([[-np.inf, 0.0],
-                                 [np.inf, 1.0]]))
+def test_uprior(data, variables, df):
+    model = SFMAModel(data, variables, True, True, df=df)
+    assert np.allclose(model.cvec, np.array([0.0, 1.0]))
 
 
 @pytest.mark.parametrize("beta", [np.arange(2)*1.0, np.ones(2)])
-def test_gradient_beta(data, variables, beta):
-    model = SFMAModel(data, variables, True, True)
+def test_gradient_beta(data, variables, df, beta):
+    model = SFMAModel(data, variables, True, True, df=df)
     my_gradient = model.gradient_beta(beta)
     tr_gradient = ad_jacobian(model.objective_beta, beta)
     assert np.allclose(my_gradient, tr_gradient)
 
 
 @pytest.mark.parametrize("beta", [np.arange(2)*1.0, np.ones(2)])
-def test_hessian_beta(data, variables, beta):
-    model = SFMAModel(data, variables, True, True)
+def test_hessian_beta(data, variables, df, beta):
+    model = SFMAModel(data, variables, True, True, df=df)
     my_hess = model.hessian_beta(beta)
     tr_hess = ad_jacobian(model.gradient_beta, beta, out_shape=(beta.size,))
     assert np.allclose(my_hess, tr_hess)
 
 
 @pytest.mark.parametrize("eta", [0.1, 1.0])
-def test_gradient_eta(data, variables, eta):
-    model = SFMAModel(data, variables, True, True)
+def test_gradient_eta(data, variables, df, eta):
+    model = SFMAModel(data, variables, True, True, df=df)
     my_gradient = model.gradient_eta(eta)
     tr_gradient = ad_jacobian(model.objective_eta, eta)
     assert np.isclose(my_gradient, tr_gradient)
 
 
 @pytest.mark.parametrize("gamma", [0.1, 1.0])
-def test_gradient_gamma(data, variables, gamma):
-    model = SFMAModel(data, variables, True, True)
+def test_gradient_gamma(data, variables, df, gamma):
+    model = SFMAModel(data, variables, True, True, df=df)
     my_gradient = model.gradient_gamma(gamma)
     tr_gradient = ad_jacobian(model.objective_gamma, gamma)
     assert np.isclose(my_gradient, tr_gradient)
