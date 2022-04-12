3eb917768d4d97206a9f26ce1a2e5dd677ce18e4
zhengp0 <zhengp@uw.edu>
2022-04-12 13:08:11 -0700

    Dynamically update the time in the documentation

--- src/sfma/__about__.py
+++ src/sfma/__about__.py
@@ -1,3 +1,5 @@
+import datetime
+
 __all__ = [
     "__title__", "__summary__", "__uri__", "__version__", "__author__",
     "__email__", "__license__", "__copyright__",
@@ -13,4 +15,4 @@ __author__ = "IHME Math Sciences"
 __email__ = "ihme.math.sciences@gmail.com"
 
 __license__ = "BSD 2-Clause License"
-__copyright__ = f"Copyright 2021 {__author__}"
+__copyright__ = f"{datetime.date.today().year} {__author__}"
